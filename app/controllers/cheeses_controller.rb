95e807ffd628a6dc80b1293d0d46ddbad084bcd4
sean-code <john.nganga@student.moringaschool.com>
2022-11-21 17:33:56 +0300

    Coded along

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,9 @@
+class CheesesController < ApplicationController
+    # def index
+    #     byebug
+    #   end
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+      end
+end
